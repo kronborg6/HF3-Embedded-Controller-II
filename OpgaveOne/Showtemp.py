040b4adfaea2fe9ad4928f21f26b7103d22c381a
Kronborg <mkronborg7@gmail.com>
2021-09-17 08:21:03 +0200

    0.5.6

--- OpgaveOne/Showtemp.py
+++ OpgaveOne/Showtemp.py
@@ -1,15 +1,18 @@
 from grovepi import *
 from grove_rgb_lcd import *
+import time
+
 
 
 def display_temp_c(temp, hum):
     c = (temp - 32) * 0.5556
-    print("")
+    time.sleep(1)
     setRGB(0, 128, 64)
     setRGB(0, 255, 0)
     setText("Temp: " + str(c) + "C       " + "Humidity: " + str(hum) + "%")
 
 def display_temp_fan(temp, hum):
+    time.sleep(1)
     setRGB(0, 128, 64)
     setRGB(0, 255, 0)
     setText("Temp: " + str(temp) + "C       " + "Humidity: " + str(hum) + "%")
