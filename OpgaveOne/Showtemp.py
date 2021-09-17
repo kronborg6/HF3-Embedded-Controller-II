5fad796b38f84340f77966f3defdf40dd52a12b9
Kronborg <mkronborg7@gmail.com>
2021-09-17 11:07:06 +0200

    0.5.23

--- OpgaveOne/Showtemp.py
+++ OpgaveOne/Showtemp.py
@@ -5,12 +5,14 @@ import time
 
 
 def display_temp_c(temp, hum):
-    setRGB(0, 128, 64)
-    setRGB(0, 255, 0)
-    setText_norefresh("Temp: " + str(temp) + "C       " + "Humidity: " + str(hum) + "%")
+    if str(temp) != "nan" and str(hum) != "nan":
+        setRGB(0, 128, 64)
+        setRGB(0, 255, 0)
+        setText_norefresh("Temp: " + str(temp) + "C       " + "Humidity: " + str(hum) + "%")
 
 def display_temp_fan(temp, hum):
-    f = temp * 1.8 + 32
-    setRGB(0, 128, 64)
-    setRGB(0, 255, 0)
-    setText_norefresh("Temp: " + str(f) + "C       " + "Humidity: " + str(hum) + "%")
+    if str(temp) != "nan" and str(hum) != "nan":
+        f = temp * 1.8 + 32
+        setRGB(0, 128, 64)
+        setRGB(0, 255, 0)
+        setText_norefresh("Temp: " + str(f) + "C       " + "Humidity: " + str(hum) + "%")
